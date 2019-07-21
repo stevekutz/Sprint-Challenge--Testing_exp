2c65b1187d15e6b232caf96466ccb4996fe774f4
stevekutz <stkutz@gmail.com>
2019-07-21 18:04:35 -0500

    added test_db & seeding for testing

--- data/migrations/20190720114533_create_games_table.js
+++ data/migrations/20190720114533_create_games_table.js
@@ -1,3 +1,6 @@
+// npx knex migrate:latest  // for dev environment
+// npx knex migrate:latest --env=testing    // for testing env
+
 
 exports.up = async function(knex) {
     await knex.schema.createTable('games', tbl => {
@@ -6,7 +9,15 @@ exports.up = async function(knex) {
         tbl.string('genre', 64).notNullable();
         tbl.integer('releaseYear');
     })
-};
+
+    await knex.schema.createTable('test_db', tbl => {
+      tbl.increments();
+      tbl.string('title', 64).notNullable();
+      tbl.string('genre', 64).notNullable();
+      tbl.integer('releaseYear');
+  })
+
+  };
 
 exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('games');
---
+++ data/seeds/002-test.js
@@ -0,0 +1,13 @@
+
+// MAKE sure you run npx knex seed:run --env=testing
+
+
+exports.seed = async function(knex) {
+  // insert the seed values
+    await knex('test_db').insert([
+      {id: 1, title: 'Pacman_TEST_db', genre: 'Arcade', releaseYear: '1980'},
+      {id: 2, title: 'Ms. Pacman_TEST_db', genre: 'Arcade', releaseYear: '1983'},
+      {id: 3, title: 'Asteroids_TEST_db', genre: 'Aracade', releaseYear: '1980'},
+      {id: 4, title: 'Space Invaders_TEST_db', genre: 'Arcade', releaseYear: '1980'},
+    ]);
+  };
